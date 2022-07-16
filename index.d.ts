bffcf183a9b44f288ca5e66a0dff24ec275e50f0
paalamugan <paalamugan@gmail.com>
2022-07-16 21:58:14 +0530

    chore: update types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,12 @@
 declare module "@single-spa-example/config" {
-    export const babelConfig: any;
-    export const postcssConfig: any;
-    export const tailwindConfig: any;
-    export const commitlintConfig: any;
+    export const babelConfig: Record<string, any>;
+    export const postcssConfig: Record<string, any>;
+    export const tailwindConfig: Record<string, any>;
+    export const commitlintConfig: Record<string, any>;
 
-    export const singleSpaReactWebpackConfig: any;
-    export const singleSpaReactBabelWebpackConfig: any;
-    export const singleSpaPostcssConfig: any;
-    export const singleSpaTailwindConfig: any;
-    export const singleSpaCommitlintConfig: any;
+    export const singleSpaReactWebpackConfig: (orgName: string, projectName: string, customizeWebpackConfig: Record<string, any>) => Record<string, any>;
+    export const singleSpaReactBabelConfig: (customizeBabelConfig: Record<string, any>) => Record<string, any>;
+    export const singleSpaPostcssConfig: (customizePostcssConfig: Record<string, any>) => Record<string, any>;
+    export const singleSpaTailwindConfig: (customizeTailwindConfig: Record<string, any>) => Record<string, any>;
+    export const singleSpaCommitlintConfig: (customizeCommitlintConfig: Record<string, any>) => Record<string, any>;
 }
